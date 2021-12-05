831c5942d6788ed62864e13ffa8e71b91dc6c7e7
Malvarix <vladlen.f@outlook.com>
2021-12-05 13:45:16 +0200

    Unnecessary controllers and files removed; FilmController is successfully developed; Basic Startup.cs dependencies defined

---
+++ STT.API/Controllers/FilmController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STT.Application.Clients.Implementations.Imdb.Models.Request;
+using STT.Application.Clients.Implementations.Imdb.Models.Response;
+using STT.Application.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace STT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilmController : ControllerBase
+    {
+        private readonly IFilmService _filmService;
+
+        public FilmController(IFilmService filmService)
+        {
+            _filmService = filmService;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetFilm))]
+        public async Task<ActionResult<SearchResponseModel>> GetFilm([FromQuery] SearchRequestModel searchRequestModel)
+        {
+            return Ok(await _filmService.GetFilmAsync(searchRequestModel));
+        }
+    }
+}
--- STT.API/Startup.cs
+++ STT.API/Startup.cs
@@ -1,9 +1,19 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using STT.Application.Clients.Implementations.Imdb;
+using STT.Application.Clients.Implementations.Imdb.Models.Request;
+using STT.Application.Clients.Implementations.Imdb.Validators;
+using STT.Application.Clients.Interfaces;
+using STT.Application.Services.Implementations;
+using STT.Application.Services.Interfaces;
+using STT.Persistence.Extensions;
 
 namespace STT.API
 {
@@ -18,7 +28,20 @@ namespace STT.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddApplicationDbContext(Configuration["STT:ConnectionString"]);
+
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                })
+                .AddFluentValidation();
+
+            services.AddTransient<IValidator<SearchRequestModel>, SearchRequestModelValidator>();
+
+            services.AddScoped<IImdbClient>(x => new ImdbClient(Configuration["ImdbApiKey"]));
+            services.AddScoped<IFilmService, FilmService>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "STT.API", Version = "v1" });
@@ -33,6 +56,8 @@ namespace STT.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "STT.API v1"));
             }
+
+            app.ApplicationServices.DatabaseMigrate();
 
             app.UseHttpsRedirection();
 
